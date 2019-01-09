9cc5d4f5526215267be20cdcea31ea7bc868563f
99vippro1 <46515618+99vippro1@users.noreply.github.com>
2019-01-09 13:41:32 +0700

    Update and rename README.md to README.cpp

---
+++ README.cpp
@@ -0,0 +1,23 @@
+# Cuong1
+bt2
+#include<iostream>
+using namespace std;
+
+ int main()
+ {
+ 	int bd;
+ 	int kv;
+ 	double ls;
+ 	float year;
+  cout<<"Nhap so tien ban dau";
+    cin>>bd;
+  cout<<"Nhap so tien ky vong";
+    cin>>kv;
+  cout<<"Nhap lai suat ";
+  	cin>>ls;
+  	year=(kv-bd)/ls;
+  cout<<"So nam de duoc so tien ky vong la"<<year;
+  
+  
+ return 0;
+ }
